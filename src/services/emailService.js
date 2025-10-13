5df3619eb2742f4d007a7670df997ed01af9e474
Silas-Oliveira <silas.oliveira8@gmail.com>
2025-10-12 21:10:15 -0300

    ajusta url

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { toast } from "react-toastify";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:3000/'
+    : 'https://mail-pj9m.onrender.com/';
+
 
 const getToken = () => {
   const token = localStorage.getItem("token");
